066fa2947f43b254f2a024777026088a4e44c5fb
Julian <yg1811@hotmail.com>
2016-07-11 14:38:50 +0800

    fixed

--- DP.py
+++ DP.py
@@ -10,19 +10,26 @@ elements in the subsequence is maximized.
 
 def maximum_subseq(A):
 
-	max_here, max_total = 0, 0
-	begin, end = 0, 0
-	for i in range(len(A)):
-		if max_here < max_here + A[i]:
-			end = i		
-		max_total = max(max_here, A[i] + max_here)
-		max_here = max(max_here, A[i])
-		if max_here < max_total:
-			begin = i
-	return begin, end, max_total
+	max_here, max_total = A[0], A[0]
+	totalBgnIdx, totalEndIdx = -1, -1
+	for i in range(1, len(A)):
+		if A[i] > A[i] + max_here:
+			max_here = A[i]
+			hereBgnIdx = i
+		else:
+			max_here = A[i] + max_here
+
+		if max_here > max_total:
+			max_total = max_here
+			totalBgnIdx = hereBgnIdx
+			totalEndIdx = i
+
+	return totalBgnIdx, totalEndIdx, max_total
 
 #print maximum_subseq([-2, -5, 6, -2, 3, -10, 5, -6])
-
+#print maximum_subseq([-2, 1, -3, 4, -1, 2, 1, -5, 4])
+#print maximum_subseq([13, -3, -25, 20, -3, -16, -23, 18, 20, 
+#	-7, 12, -5, -22, 15, -4, 7])
 
 """
 Longest Increasing Subsequence. Given a sequence of n real
@@ -33,15 +40,31 @@ values in the subsequence form a strictly increasing sequence.
 
 def longest_increasing_seq(A):
 
-	T= [1] * len(A)
+	D, T = [1] * len(A), [-1] * len(A)
+	maxLen, maxIdx = 1, -1
 	for i in range(1, len(A)):
 		for j in range(i):
-			if A[i] > A[j]:
-				T[i] = max(T[i], T[j] + 1)
+			if A[i] > A[j] and D[i] < D[j] + 1:
+				D[i] = D[j] + 1
+				T[i] = j
+		if D[i] > maxLen:
+			maxLen = D[i]
+			maxIdx = i
+	l = get_idx(maxIdx, T, [])
+	return maxLen, [A[i] for i in l]
 
-	return max(T)
+def get_idx(i, T, l):
+	if T[i] == -1:
+		l = [i] + l
+		return l
+	else:
+		l = [i] + l
+		return get_idx(T[i], T, l)
 
-print longest_increasing_seq([-2, -5, 6, -2, 3, -10, 5, -6])
+# print longest_increasing_seq([-2, -5, 6, -2, 3, -10, 5, -6])
+# print longest_increasing_seq([-3, 1, 2, 4, -6, 5, 7, 2])
+# print longest_increasing_seq([13, -3, -25, 20, -3, -16, -23, 18, 20, 
+# 	-7, 12, -5, -22, 15, -4, 7])
 
 """
 You are given n types of coin denominations of values 
