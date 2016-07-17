70c95b1a0c72959211326a79da1aff990d2d48cf
Julian <yg1811@hotmail.com>
2016-07-17 14:05:16 +0800

    minimal palindrome

--- DP.py
+++ DP.py
@@ -1,5 +1,6 @@
 # A collection of python implementation for Dynamic Programming problems
 
+import numpy as np
 
 """
 Maximum Value Contiguous Subsequence. Given a sequence 
@@ -76,7 +77,65 @@ of money C with as few coins as possible.
 
 def make_change(v, c):
 
+
+
 	pass
 
+
+def longest_common_seq(s1, s2):
+
+	pass
+	
+def edit_distance(s1, s2):
+
+	pass
+
+def minimal_palindrome(p):
+
+	n = len(p)
+	w = np.ones((n, n), dtype='int')
+	div = []
+
+	for sublen in range(1, n + 1):
+		for bgnIdx in range(n - sublen + 1):
+			endIdx = bgnIdx + sublen - 1
+			if isPalindrome(p[bgnIdx:endIdx+1]):
+				w[bgnIdx][endIdx] = 1
+			else:
+				w[bgnIdx][endIdx] = sublen
+				for midIdx in range(bgnIdx, endIdx):
+					if w[bgnIdx][endIdx] >= w[bgnIdx][midIdx] + w[midIdx+1][endIdx]:
+						div.append(((bgnIdx, midIdx), (midIdx+1, endIdx)))
+						w[bgnIdx][endIdx] = w[bgnIdx][midIdx] + w[midIdx+1][endIdx]
+	div.reverse()
+	return w[0][-1], track_palindrome((0, len(p)-1), div, w, p)
+
+
+def track_palindrome(i, I, W, P):
+	if W[i] == 1:
+		return [P[i[0]:i[1]+1]]
+	else:
+		for it in I:
+			leftIdx, rightIdx = it[0], it[1]
+			if leftIdx[0] == i[0] and rightIdx[1] == i[1]:
+				l = track_palindrome(leftIdx, I, W, P)
+				r = track_palindrome(rightIdx, I, W, P)
+				return l + r
+
+def isPalindrome(s):
+
+	if len(s) < 2:
+		return True
+	else:
+		if s[0] == s[-1]:
+			return isPalindrome(s[1:-1])
+		return False
+
+# print minimal_palindrome('bubbaseesabanana')	#3
+# print minimal_palindrome('baseesab') #1
+# print minimal_palindrome('ooxx')	#2
+# print minimal_palindrome('ababbbabbababa') #4
+# print minimal_palindrome('axffde')	#5
+# print minimal_palindrome('oxxo')	#1
 
 
